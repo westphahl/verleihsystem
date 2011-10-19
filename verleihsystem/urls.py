3e44381eae62075bd37e77b4573dc4e34ed503af
Michael Wydler <michael.wydler@googlemail.com>
2011-10-19 20:51:49 +0200

    Add category index view.

--- verleihsystem/urls.py
+++ verleihsystem/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'verleihsystem.views.home', name='home'),
     # url(r'^verleihsystem/', include('verleihsystem.foo.urls')),
 
+    url(r'^category/$', 'categories.views.index'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
