d878fcbf83f23bd051acf1127fbb27d4bc9f21be
Simon Westphahl <westphahl@gmail.com>
2011-10-20 23:28:15 +0200

    Adds slug field to category.

--- verleihsystem/categories/models.py
+++ verleihsystem/categories/models.py
@@ -1,12 +1,13 @@
-import mptt
 from django.db import models
-from mptt.models import MPTTModel, TreeForeignKey
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+from mptt.models import MPTTModel, TreeForeignKey
+
+
 class Category(MPTTModel):
     name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
     contact = models.ForeignKey(User, blank=True, null=True, 
         on_delete=models.SET_NULL, 
         limit_choices_to={'groups__name' : 'Verwalter'})
