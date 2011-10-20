5baa4fb91d9b6a80d748b47192b91e2a68b567d0
Michael Wydler <michael.wydler@googlemail.com>
2011-10-20 08:46:36 +0200

    Add request to return value.

--- verleihsystem/categories/views.py
+++ verleihsystem/categories/views.py
@@ -4,4 +4,4 @@ from categories.models import Category
 def index(request):
     category_list = Category.tree.all()
     return render_to_response("home.html",
-        {'nodes': category_list})
+        {'nodes': category_list, 'request': request})
