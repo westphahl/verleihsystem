78ff75cbcaac75ffb411d6c530c6a031d4f12d5a
Simon Westphahl <westphahl@gmail.com>
2011-10-24 11:49:30 +0200

    Adds optional picture to user profile (inkluding dependencies and upload dir).

--- verleihsystem/accounts/models.py
+++ verleihsystem/accounts/models.py
@@ -3,6 +3,12 @@ from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 
 
+def get_photo_path(instance, filename):
+    upload_dir = 'accounts/'
+    path, ext = filename.split('.')
+    return upload_dir + instance.user.username + '.' + ext
+
+
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     student_number = models.PositiveIntegerField(blank=True, null=True,
@@ -11,7 +17,7 @@ class UserProfile(models.Model):
         verbose_name=_("Phone"))
     mobile_phone = models.CharField(blank=True, max_length=100,
         verbose_name=_("Mobile phone"))
-    picture = models.ImageField(blank=True, upload_to='accounts',
+    picture = models.ImageField(blank=True, upload_to=get_photo_path,
         verbose_name=_("Photo"))
 
     class Meta:
