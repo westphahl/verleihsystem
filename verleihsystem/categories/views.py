b77e7d99b97e201382575f871eca68865b3b65fe
Simon Westphahl <westphahl@gmail.com>
2011-10-25 11:09:47 +0200

    Fixes bug with recent version of django-mptt.

--- verleihsystem/categories/views.py
+++ verleihsystem/categories/views.py
@@ -5,7 +5,7 @@ from products.models import ProductType
 
 
 def index(request):
-    category_list = Category.tree.all()
+    category_list = Category.objects.all()
     return render_to_response("home.html",
         {'nodes': category_list},
         context_instance=RequestContext(request))
