64af5989aa095ba59c8023971fc67f7fa0c00ff6
Michael Wydler <michael.wydler@googlemail.com>
2011-11-03 22:26:14 +0100

    Change category and producttype styles.

--- verleihsystem/categories/views.py
+++ verleihsystem/categories/views.py
@@ -5,7 +5,6 @@ from products.models import ProductType
 
 
 def index(request):
-    category_list = Category.objects.all()
     return render_to_response("home.html",
         context_instance=RequestContext(request))
 
