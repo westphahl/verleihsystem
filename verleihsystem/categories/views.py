1d93693bc4a5b6e0c4b44e4e93d0025d70749baa
Michael Wydler <michael.wydler@googlemail.com>
2011-12-01 09:27:10 +0100

    Move category_tree from context processor to view.

--- verleihsystem/categories/views.py
+++ verleihsystem/categories/views.py
@@ -13,8 +13,33 @@ def index(request):
 def category_detail(request, path, template, *args, **kwargs):
     leaf = get_object_or_404(Category, path=path)
     
+    tree = []
+    tree.append(leaf)
+    for c in leaf.get_children():
+        tree.append(c)
+    
+    last_leaf = leaf
+    for a in leaf.get_ancestors(True):
+        tmp = []
+        tmp.append(a)
+        for cc in a.get_children():
+            if last_leaf == cc:
+                tmp.extend(tree)
+                last_leaf = a;
+            else:
+                tmp.append(cc)
+        tree = tmp
+    
+    tmp = []
+    for r in last_leaf.get_siblings(True):
+        if r == last_leaf:
+            tmp.extend(tree)
+        else:
+            tmp.append(r)
+    tree = tmp
+    
     sub_tree = leaf.get_descendants(include_self=True)
     product_list = ProductType.objects.filter(categories__in=sub_tree
         ).annotate(product_count=Count('product', distinct=True))
-    return render_to_response(template, {'category': leaf,
+    return render_to_response(template, {'category': leaf, 'tree_nodes': tree,
         'product_list': product_list}, context_instance=RequestContext(request))
