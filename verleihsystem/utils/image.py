d332e42e0f985d93a8359e269f154e8df97e98cf
Simon Westphahl <westphahl@gmail.com>
2011-12-04 20:14:47 +0100

    Adds documentation for image utils module.

--- verleihsystem/utils/image.py
+++ verleihsystem/utils/image.py
@@ -3,26 +3,29 @@ import math
 
 def entropy(image):
     """
-    Calculate the entropy of a given image.
+    Calculates the entropy of a given image.
 
-    Entropie ist ein Mass fuer die Informationsdichte (mittlerer Informations-
-    gehalt) eines Zeichensatzes, wobei hier ein Pixel ein Zeichen ist.
+    Entropy is a measure of the average information density of a symbol system.
+    In this case the symbols are pixels.
 
-    > http://www.reddit.com/r/pics/comments/j1q8q/for_science/
-    The entropy of a image uses a structure in image processing known as a
-    histogram. You can think of a histogram as a graph where the x-axis
-    represents the range of all color intensities and the y-axis represents the
-    frequency each intensity occurs in the image.
-    The function returns a high value if there are a lot of different color
-    intensities in the image, and a low value if there are a lot of similar
-    color intensities.
+    See: http://en.wikipedia.org/wiki/Entropy_(information_theory)
+    
+        "The entropy of a image uses a structure in image processing known as a
+        histogram. You can think of a histogram as a graph where the x-axis
+        represents the range of all color intensities and the y-axis
+        represents the frequency each intensity occurs in the image. The
+        function returns a high value if there are a lot of different color
+        intensities in the image, and a low value if there are a lot of
+        similar color intensities."
+
+    Source: http://www.reddit.com/r/pics/comments/j1q8q/for_science/c28fpb7
     """
 
     # Get the distribution of colors in the image.
     # 
-    # That is the number of pixels that have colors in each of a fixeed list of
-    # color ranges, that span the image's color space, the set of all possible
-    # colors.
+    # That's the number of pixels that have colors in each of a fixed list of
+    # color ranges, that span the image's color space.
+    # (color space = the set of all possible colors).
     histogram = image.histogram()
 
     # Calculate the scope of the histogram
@@ -39,19 +42,22 @@ def entropy(image):
     
     # Calculate relative frequencies for non-zero bins
     #
-    # A bin holds the number of pixels for a intensity range of a color channel.
-    # Python's PIL divides a color channel into 256 intensity ranges.
+    # A bin holds the number of pixels for a intensity range of a color
+    # channel. Python's PIL divides a color channel into 256 intensity ranges.
     relative_frequencies = [c / histogram_scope for c in histogram if c != 0]
 
+    # Return the entropy of the image.
     return -sum([f * math.log(f, 2) for f in relative_frequencies])
 
 
 def scale_image(image_path, max_width, max_height, crop=True):
     """
-    Scale image, optionally cropping it to force the image to the specified
-    width and height.
+    Scale image, optionally cropping it to the specified width and height.
 
-    .. function:: scale_image(image_path, max_width, max_height[, crop=False])
+    The function takes a path to an image as it's first argument and returns
+    a PIL Image file.
+    If crop is set to false the aspect ratio of the source image is preserved 
+    and the result contains a image not larger than the given width and height.
     """
     from PIL import Image
 
@@ -60,6 +66,7 @@ def scale_image(image_path, max_width, max_height, crop=True):
     src_aspect_ratio = float(width) / height
     dst_aspect_ratio = float(max_width) / max_height
 
+    # Crop the source image to match the desired aspect ratio.
     if crop:
         if src_aspect_ratio > dst_aspect_ratio: 
             # Cut the sides off.
@@ -72,7 +79,7 @@ def scale_image(image_path, max_width, max_height, crop=True):
                 left = img.crop((0, 0, slice_width, y))
                 right = img.crop((x - slice_width, 0, x, y))
 
-                #remove the slice with the least entropy
+                # Remove the slice with the least entropy
                 if entropy(left) < entropy(right):
                     img = img.crop((slice_width, 0, x, y))
                 else:
@@ -90,14 +97,15 @@ def scale_image(image_path, max_width, max_height, crop=True):
                 top = img.crop((0, 0, x, slice_height))
                 bottom = img.crop((0, y - slice_height, x, y))
 
-                #remove the slice with the least entropy
+                # Remove the slice with the least entropy
                 if entropy(top) < entropy(bottom):
                     img = img.crop((0, slice_height, x, y))
                 else:
                     img = img.crop((0, 0, x, y - slice_height))
 
                 crop_height = crop_height - 10
-     
+
+    # Do the actual scaling of the image
     if width < height:
         img.thumbnail((max_height, max_width), Image.ANTIALIAS)
     else:
