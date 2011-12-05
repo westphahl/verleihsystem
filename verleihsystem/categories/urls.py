e74f176fffaefb8adf92f001c084124689cfaa3f
Simon Westphahl <westphahl@gmail.com>
2011-12-05 14:49:06 +0100

    Moves to a generic view for the homepage.

--- verleihsystem/categories/urls.py
+++ verleihsystem/categories/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 from categories.views import category_detail
 
+
 urlpatterns = patterns('',
     url(r'^(?P<path>(?:[\w-]+/)+)$', category_detail,
         {'template': 'categories/category_detail.html'},
--- verleihsystem/urls.py
+++ verleihsystem/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic.base import TemplateView
+
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'categories.views.index', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^contact/$', 'contact.views.contact_form', name='contact_form'),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^category/', include('categories.urls')),
