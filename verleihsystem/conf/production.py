0d53ed70ee76ce2dc1a545a9c426365aa31c92a8
Simon Westphahl <westphahl@gmail.com>
2011-12-07 10:16:01 +0100

    Adds config for production database.

--- verleihsystem/conf/production.py
+++ verleihsystem/conf/production.py
@@ -1,5 +1,5 @@
 # Django settings for verleihsystem project.
-from conf.production import *
+from conf.default import *
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -8,21 +8,25 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'verleihsystem',                      # Or path to database file if using sqlite3.
+        'USER': 'verleihsystem',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': 'neptun.fbe.fh-weingarten.de',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = '/usr/webapps/verleihsystem/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'pluto.hs-weingarten.de/verleihsystem/media/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = ''
+ADMIN_MEDIA_PREFIX = 'pluto.hs-weingarten.de/verleihsystem/media/admin/'
