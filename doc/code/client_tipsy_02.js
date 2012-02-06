a659525a880323abc5957ae3bf07fb4249ce1840
Michael Wydler <michael.wydler@googlemail.com>
2012-02-06 12:18:02 +0100

    Update documetation.

--- doc/code/client_tipsy_02.js
+++ doc/code/client_tipsy_02.js
@@ -1,7 +1,7 @@
 $(function() {
-    $('*[rel=tipsy_n]').tipsy({delayIn: 500, gravity: 'n'});
-    $('*[rel=tipsy_w]').tipsy({fade: true, gravity: 'w'});
-    $('*[rel=tipsy_s]').tipsy({delayIn: 500, gravity: 's'});
-    $('*[rel=tipsy_e]').tipsy({fade: true, gravity: 'e'});
-    $('*[rel=tipsy_adminopt]').tipsy({delayIn: 500, fade: true, gravity: 'n'});
+    $('.tipsy_n').tipsy({delayIn: 500, gravity: 'n'});
+    $('.tipsy_w').tipsy({fade: true, gravity: 'w'});
+    $('.tipsy_s').tipsy({delayIn: 500, gravity: 's'});
+    $('.tipsy_e').tipsy({fade: true, gravity: 'e'});
+    $('.tipsy_adminopt').tipsy({delayIn: 500, fade: true, gravity: 'n'});
 });
