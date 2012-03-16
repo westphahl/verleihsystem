1c897c316df4d01b199d2ea7f0cc61857c32e56f
Michael Wydler <michael.wydler@googlemail.com>
2012-03-16 21:33:20 +0100

    Add symlink to media/download.

--- doc/code/installation_install_media_01.sh
+++ doc/code/installation_install_media_01.sh
@@ -1,5 +1,7 @@
 $ ln -s /usr/webapps/verleihsystem/mount/prj/verleihsystem/media/css media/css
 $ ln -s /usr/webapps/verleihsystem/mount/prj/verleihsystem/media/img media/img
 $ ln -s /usr/webapps/verleihsystem/mount/prj/verleihsystem/media/js media/js
+$ ln -s /usr/webapps/verleihsystem/mount/prj/verleihsystem/media/download 
+    media/download
 $ ln -s /usr/webapps/verleihsystem/environment/lib/python2.7/site-packages/
     django/contrib/admin/media/ media/admin
